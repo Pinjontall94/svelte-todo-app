f92a96bf288839b277f02d15862e4bfe069b3d28
Sam Johnson <sambjohnson94@gmail.com>
2022-05-24 19:41:30 -0700

    working demo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,68 @@
 <script>
     
     import { onMount } from 'svelte';
-    
+   
+    // Initialize an empty array for the todos 
+    //  and an emtpy string as the todo text
+    let todos = [];
+    let todoText = '';
 
+    // Create event listener to update todos array on mount (if needed)
+    onMount(() => {
+        const existingTodos = localStorage.getItem('todos');
+        todos = JSON.parse(existingTodos) || [];
+    });
+
+    function addTodo() {
+        todos = [...todos, todoText];
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }
 
 </script>
 
-<main>
-
-    <h1>Svelte Todo App</h1>
+<main class="center">
     
-    <form></form>
+    <h1>Svelte Todo App 🍊</h1>
     
-    <ul></ul>
+    <form on:submit|preventDefault={addTodo}>
+        <input bind:value={todoText} placeholder="What needs to get done?">
+        <input type="submit" value="Add todo">
+    </form>
+    
+    <ul>
+        {#each todos as todo}
+        <li>{todo}</li>
+        {/each}
+    </ul>
 
 </main>
 
-<style></style>
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Overpass&display=swap');
+main {
+    font-family: 'Overpass', sans-serif;
+    font-size: 20px;
+}
+
+:global(body) {
+    background-color: #f6fafd;
+}
+
+.center {
+    text-align: center;
+}
+
+h1 { 
+    color: #ff3e00; 
+}
+
+ul {
+    display: inline-block;
+    text-align: left;
+    list-style-position: outside;
+    font-size: 1.4em;
+    color: #444444;
+}
+
+</style>
